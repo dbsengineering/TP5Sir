7de3eaa3818d83397ad599f543f35bbf15fcb25c
DbsEngineering <piranna56@yahoo.fr>
2018-01-20 12:50:23 +0100

    Question1
    modify index and make question1

--- js/interaction.js
+++ js/interaction.js
@@ -2,11 +2,23 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
+
+	//Question 1 Déclarer les 4 attributs
+	this.coordInitX = 0; // Coordonnées X initiale
+	this.coordInitY = 0; // Coordonnée Y initiale
+	this.coordFinX = 0; // Coordonnée X de fin
+	this.coordFinY = 0; // 
+
 	// Définir ici les attributs de la 'classe'
 
 	// Developper les 3 fonctions gérant les événements
 
 	// Associer les fonctions précédentes aux évènements du canvas.
+};
+
+
+function even1(evt){
+
 };
 
 
