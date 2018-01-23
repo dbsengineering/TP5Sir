38d655bbef23b985c7c17f4ab8833928d2b2eac6
DbsEngineering <piranna56@yahoo.fr>
2018-01-23 13:45:37 +0100

    add model form
    add model form Question 6

--- js/model.js
+++ js/model.js
@@ -1,3 +1,33 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+// Classe Drawing
+function Drawing() {
+    this.monTableau = new Array();
+    
+};
+
+// Classe Forme
+function Forme(couleur, epaisseur){
+    this.couleur = couleur;
+    this.epaisseur = epaisseur;
+};
+
+// classe Rectangle extends Forme
+function Rectangle(coordHGX, coordHGY, largeur, hauteur){
+    Forme.call();
+    this.coordHGX = coordHGX;
+    this.coordHGY = coordHGY;
+    this.largeur = largeur;
+    this.hauteur = hauteur;
+};
+
+// Classe Line extends Forme
+function Line(coordSX, coordSY, coordEX, coordEY){
+    Forme.call();
+    this.coordSX = coordSX;
+    this.coordSY = coordSY;
+    this.coordEX = coordEX;
+    this.coordEY = coordEY;
+};
