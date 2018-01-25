f998221e1a443fd88b4fbc1cc152ba017b836672
DbsEngineering <piranna56@yahoo.fr>
2018-01-25 08:01:40 +0100

    add Q7

--- js/model.js
+++ js/model.js
@@ -5,22 +5,56 @@
 // Classe Drawing
 function Drawing() {
     this.monTableau = new Array();
-    
+    //Fonction paint Q7
+    this.paint = function(canvas){
+        canvas.fillStyle = '#F0F0F0'; // set canvas' background color
+        canvas.fillRect(0, 0, canvas.width, canvas.height);  // now fill the canvas
+    }
 };
 
 // Classe Forme
 function Forme(couleur, epaisseur){
     this.couleur = couleur;
     this.epaisseur = epaisseur;
+
+    //Fonction paint Q7
+    this.paint = function(canvas){
+            canvas.strokeWeight(this.epaisseur);
+            canvas.strokeStyle(this.couleur);
+    };
+
+    // Fonction qui retourne la couleur de la forme
+    this.getColor = function(){
+        return this.couleur;
+    };
+
 };
 
 // classe Rectangle extends Forme
 function Rectangle(coordHGX, coordHGY, largeur, hauteur){
     Forme.call();
-    this.coordHGX = coordHGX;
-    this.coordHGY = coordHGY;
-    this.largeur = largeur;
-    this.hauteur = hauteur;
+    //Initialisation des propriétées
+    this.coordHGX = coordHGX; // Coordonée X
+    this.coordHGY = coordHGY; // Coordonnée Y
+    this.largeur = largeur; // Largeur du rectangle
+    this.hauteur = hauteur; // Hauteur du rectangle
+
+    //Fonction qui retourne la coordonnée de X
+    this.getInitX = function(){
+        return this.coordHGX;
+    }
+    //Fonction qui retourne la coordonnée de Y
+    this.getInitY = function(){
+        return this.coordHGY;
+    }
+    //Fonction qui retourne la coordonnée de X final
+    this.getFinalX = function(){
+        return (this.coordHGX + this.largeur);
+    }
+    //Fonction qui retourne la coordonnée de Y final
+    this.getFinalY = function(){
+        return (this.coordHGY + this.hauteur);
+    }
 };
 
 // Classe Line extends Forme
